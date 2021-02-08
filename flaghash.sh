6653811e29ead9e61b041abf8ffe72f800bf4ece
magpie-inc <76676738+magpie-inc@users.noreply.github.com>
2021-02-07 19:42:06 -0700

    Fix auth!!

--- flaghash.sh
+++ flaghash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-FLAG=$(curl -b 'auth=f4e53e561c6580d6d304f3f31e3102f5' http://srv3.magpiectf.ca:9355/api.php | tr -d " \t\n\r" )
+FLAG=$(curl -b "auth=$AUTH" http://srv3.magpiectf.ca:9355/api.php | tr -d " \t\n\r" )
 echo $( md5sum <<< $FLAG | cut -d " " -f1)
 
